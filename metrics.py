6432b3e3792893bc434721f9652647cc3427fa1e
ClairvoyantCoder <himangshu.lahkar@iitgn.ac.in>
2025-08-22 14:51:07 +0530

    Updated file utils and metrics

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 from typing import Union
 import pandas as pd
-
+import numpy as np
 
 def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     """
@@ -12,35 +12,54 @@ def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     Students are required to add appropriate assert checks at places to
     ensure that the function does not fail in corner cases.
     """
-    assert y_hat.size == y.size
-    # TODO: Write here
-    pass
+    assert y_hat.size == y.size, "predictions and labels must have same size"
+    if y.size==0:
+        return 0.0 #corner case: empty input
+    correct = (y_hat == y).sum()
 
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the precision
     """
-    pass
+    assert y_hat.size == y.size
+    tp = ((y_hat == cls) & (y == cls)).sum()
+    fp = ((y_hat == cls) & (y != cls)).sum()
+
+    if tp + fp == 0: #avoid division by zero
+        return 0.0
+    return tp/(tp+fp)
 
 
 def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the recall
     """
-    pass
+    assert y_hat.size == y.size
+    tp = ((y_hat == cls) & (y == cls)).sum()
+    fn = ((y_hat != cls) & (y == cls)).sum()
+    
+    if tp + fn == 0: 
+        return 0.0
+    return tp / (tp + fn)
 
 
 def rmse(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the root-mean-squared-error(rmse)
     """
-
-    pass
+    assert y_hat.size == y.size
+    if y.size == 0:
+        return 0.0
+    return np.sqrt(((y_hat - y) ** 2).mean())
 
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the mean-absolute-error(mae)
     """
-    pass
+    assert y_hat.size == y.size
+    if y.size == 0:
+        return 0.0
+    
+    return (y_hat - y).abs().mean()
--- tree/utils.py
+++ tree/utils.py
@@ -4,44 +4,64 @@ There is no restriction on following the below template, these fucntions are her
 """
 
 import pandas as pd
+import numpy as np
 
 def one_hot_encoding(X: pd.DataFrame) -> pd.DataFrame:
     """
     Function to perform one hot encoding on the input data
     """
-
-    pass
+    return pd.get_dummies(X)
 
 def check_ifreal(y: pd.Series) -> bool:
     """
     Function to check if the given series has real or discrete values
     """
-
-    pass
+    return pd.api.types.is_float_dtype(y)
 
 
 def entropy(Y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
-
-    pass
+    probs = Y.value_counts(normalize=True)
+    return -np.sum(probs*np.log2(probs+1e-9))
 
 
 def gini_index(Y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
-
-    pass
+    probs = Y.value_counts(normalize=True)
+    return 1-np.sum(probs**2)
 
 
 def information_gain(Y: pd.Series, attr: pd.Series, criterion: str) -> float:
     """
     Function to calculate the information gain using criterion (entropy, gini index or MSE)
     """
+    if criterion == "entropy":
+        base = entropy(Y)
+    elif criterion == "gini":
+        base = gini_index(Y)
+    elif criterion == "mse":
+        base = np.var(Y) #variance = mse before the split
+    else:
+        raise ValueError
+    
+    #weighted impurity after split
+    values = attr.unique()
+    weighted_impurity = 0
+    for v in values:
+        mask = (attr == v)
+        Y_v = Y[mask]
+        if criterion == "entropy":
+            weighted_impurity += (len(Y_v) / len(Y)) * entropy(Y_v)
+        elif criterion == "gini":
+            weighted_impurity += (len(Y_v) / len(Y)) * gini_index(Y_v)
+        elif criterion == "mse":
+            weighted_impurity += (len(Y_v) / len(Y)) * np.var(Y_v)
+    return base-weighted_impurity
 
-    pass
 
 
 def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
@@ -56,8 +76,27 @@ def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.S
     """
 
     # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
+    best_gain = -1
+    best_attr = None
+    best_value = None
 
-    pass
+    for attr in features:
+        if pd.api.types.is_numeric_dtype(X[attr]):  # real-valued feature
+            unique_vals = X[attr].unique()
+            unique_vals.sort()
+            # try midpoints between sorted values
+            thresholds = (unique_vals[:-1] + unique_vals[1:]) / 2
+            for t in thresholds:
+                mask = X[attr] <= t
+                gain = information_gain(y, mask, criterion)  # mask as "attribute"
+                if gain > best_gain:
+                    best_gain, best_attr, best_value = gain, attr, t
+        else:  # discrete/categorical feature
+            gain = information_gain(y, X[attr], criterion)
+            if gain > best_gain:
+                best_gain, best_attr, best_value = gain, attr, None
+
+    return best_attr, best_value, best_gain
 
 
 def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
@@ -74,4 +113,13 @@ def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
 
     # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
 
-    pass
+    if value is None:  # categorical split
+        splits = {}
+        for v in X[attribute].unique():
+            mask = (X[attribute] == v)
+            splits[v] = (X[mask], y[mask])
+        return splits
+    else:  # real-valued split
+        left_mask = (X[attribute] <= value)
+        right_mask = (X[attribute] > value)
+        return (X[left_mask], y[left_mask]), (X[right_mask], y[right_mask])
